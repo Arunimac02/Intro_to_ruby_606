101a0a416582152b0c456dcba62076727a97a6cd
arunimac02 <chowdh36@purdue.edu>
2025-01-27 22:25:32 -0600

    completed max_2_sum with all correct test cases

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -9,7 +9,21 @@ end
 
 def max_2_sum(arr)
   # YOUR CODE HERE
-  0
+  return 0 if arr.empty?
+  return arr[0] if arr.length == 1
+  max_a = -Float::INFINITY
+  max_b = -Float::INFINITY
+  
+  arr.each do |num|
+    if num > max_a
+      max_b = max_a
+      max_a = num
+      max_a_idx = arr.find_index(max_a)
+    elsif num > max_b and arr.find_index(num) != max_a_idx
+      max_b = num
+    end
+  end
+  max_a + max_b
 end
 
 def sum_to_n?(arr, number)
